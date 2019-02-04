c0b231a35444856e9b46ca01d3a9b718dbeaedca
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-02-04 10:39:33 +0200

    Create import.py

---
+++ import.py
@@ -0,0 +1,25 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# check for DATABASE_URL environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+# set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+	# import data from "books.csv" into "books" table
+	with open("books.csv") as file:
+		reader = csv.reader(file)
+		for isbn, title, author, year in reader:
+			db.execute("""INSERT INTO books (isbn, title, author, year) 
+			              VALUES (:isbn, :title, :author, :year)""",
+			{"isbn":isbn, "title":title, "author":author, "year":year})
+		db.commit()
+
+if __name__ == "__main__":
+	main()
